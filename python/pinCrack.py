5e5c9037bc9aa9f8b39a06aaee2aad56b99916d2
Barikuma <barikumamondelo@gmail.com>
2022-12-14 20:24:49 +0100

    pinCrack update

--- python/pinCrack.py
+++ python/pinCrack.py
@@ -10,12 +10,5 @@ for i in range(len(string_pin)):
         value = str(randint(0, 9))
         if value == string_pin[i]:
             crack.append(value)
-            print(value)
             new_pin = ''.join(crack)
             break
-
-print(new_pin)
-
-if new_pin == pin:
-    print("CRACKED!")
-    print(int(new_pin))
